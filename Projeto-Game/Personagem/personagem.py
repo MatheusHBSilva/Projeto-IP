d12b342bc77fabc2ffcc6f47bd3c0d6222806395
Matheus Henrique Barros da Silva <66789359+MatheusHBSilva@users.noreply.github.com>
2023-09-11 08:21:37 -0300

    Update da mecânica de pulo e wall sliding

--- Projeto-Game/Personagem/personagem.py
+++ Projeto-Game/Personagem/personagem.py
@@ -5,16 +5,15 @@ class player:
     def __init__(self):
         self.player_sprite = pygame.image.load('personagem/personagem_placeholder.png').convert_alpha() #imagem da personagem//quando colocarmos animação, terá que ser modificado
         self.player_sprite = pygame.transform.scale(self.player_sprite, (40, 55)) #resize do tamanho da imagem da personagem
-        self.x = 100 #x iniciai da personagem
+        self.x = 100 #x inicial da personagem
         self.y = 700 #y inicial da personaem
         self.y_plataforma = self.y #esse y será utilizado para modidicar o y que ela vai descer em relação a plataforma
         self.velocidade = 5
-        self.forca_pulo = 30
+        self.forca_pulo = 20
         self.double_jump_available = False #verifica se é possível dar o double jump nesse momento
         self.double_jump_reset = True #reseta a possibilidade de dar o double jump (essa variável é confusa, ela é mais uma bugfixer)
         self.is_jumping = False #verifica se a personagem esta no ar 
         self.gravidade = 1 #força de gravidade do projeto
-        self.gravidade_sliding = 0.2
         self.sliding = False
         self.jump_height = 0
         self.fall = False
@@ -23,17 +22,22 @@ class player:
 
     def movimento(self):
         #siatema de pulo
-        keys = pygame.key.get_pressed() #verifica se a tecla 'espaço' foi pressionada
+        keys = pygame.key.get_pressed() #verifica se a tecla 'space' foi pressionada
 
         if(keys[pygame.K_SPACE] and not self.is_jumping): #pulo normal inicial, liberando o código de aumentar a altura
             self.is_jumping = True 
             self.jump_height = 0
 
-        if(keys[pygame.K_SPACE] and self.double_jump_available and self.fall and self.double_jump_reset): #verifica o double jump // o double jump só é liberado caso a altura máxima aconteça, mas é possível mudar essa variável tranquilamente
+        if(keys[pygame.K_SPACE] and self.double_jump_available and self.fall and self.double_jump_reset): #verifica o double jump // o double jump só é liberado caso a altura máxima aconteça ou caso o personagem está dando sliding, mas é possível mudar essa variável tranquilamente
             self.double_jump_available = False
             self.double_jump_reset = False
             self.fall = False
             self.jump_height = 0
+            self.is_jumping = True
+
+            if(self.sliding == True):
+                self.velocidade *= -1
+                self.sliding = False
        
         if(self.is_jumping and not self.fall): #aumenta a altura do personagem no eixo Y
             self.y -= self.forca_pulo - self.jump_height
@@ -44,7 +48,8 @@ class player:
                 self.jump_height = 0
 
         if(self.fall): #o código de gravidade, bastante semelhante ao código de subida   
-            if(self.x == 0 or self.x >= 395): #caso o personagem esteja nas paredes (beirada do mapa), ele entrará em slide pelo código abaixo e sua velocidade de descida será menor
+            if(self.x <= 0 or self.x >= 395): #caso o personagem esteja nas paredes (beirada do mapa), ele entrará em slide pelo código abaixo e sua velocidade de descida será menor
+                self.sliding = True
                 self.jump_height = 0
                 self.jump_height += 1.9
                 self.y += 0.5 + self.jump_height
@@ -61,15 +66,16 @@ class player:
                 self.y = self.y_plataforma
                 self.double_jump_reset = True
 
-        #código de controle automático da movimentação no x + slide
+        #código de controle automático da movimentação no x + slide wall
         #OBS: o slide está funcionando da seguinte forma, eu limitei a distância máxima que a personagem pode andar para 0-395, quando ela chega nessas coordenadas, inverte sua velocidade
         #porém, a velocidade só será invertida se o personagem não estiver caindo, caso ele esteja, apenas muda o eixo da imagem
         if(self.velocidade > 0): #caso a velocidade seja positiva, ou seja, indo para direita
             if(self.x != 432):
                 if(self.x < 395):
                     self.x += self.velocidade
-            if(self.x >= 395 and not self.fall):
+            if(self.x >= 395 and self.y == self.y_plataforma):
                 self.velocidade *= -1
+                self.sliding = False
             if(self.x >= 395 and self.inverter_direita):
                 self.player_sprite = pygame.transform.flip(self.player_sprite, True, False)
                 self.inverter_direita = False
@@ -78,8 +84,9 @@ class player:
         if(self.velocidade < 0): #velocidade negativa (indo para a esquerda)
             if(self.x != 0):
                 self.x += self.velocidade
-            if(self.x <= 0 and not self.fall): 
+            if(self.x <= 0 and self.y == self.y_plataforma): 
                 self.velocidade *= -1
+                self.sliding = False
             if(self.x <= 0 and self.inverter_esquerda):
                 self.player_sprite = pygame.transform.flip(self.player_sprite, True, False)
                 self.inverter_esquerda = False
@@ -89,5 +96,3 @@ class player:
         tela = pygame.display.get_surface()
         tela.fill((255, 0, 0))
         tela.blit(self.player_sprite, (self.x, self.y))
-
-        print(self.velocidade)
