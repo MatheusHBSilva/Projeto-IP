f86a96896f45763324ce6d13e8f73758731b5016
Matheus Henrique Barros da Silva <66789359+MatheusHBSilva@users.noreply.github.com>
2023-09-30 20:21:50 -0300

    Add files via upload

--- inimigos/canhão/canhao.py
+++ inimigos/canhão/canhao.py
@@ -11,7 +11,6 @@ class Canhao:
         self.velocidades = []
         self.lane = -1
 
-        self.hitting = False
         self.sprite_bala = pygame.image.load('inimigos/canhão/tiro_placeholder.png').convert_alpha()
         self.sprite_bala = pygame.transform.scale(self.sprite_bala, (15, 25))
         self.sprite_canhao = pygame.image.load('inimigos/canhão/canhão.png').convert_alpha()
@@ -22,24 +21,25 @@ class Canhao:
 
         self.collider_bala = pygame.Rect(self.x_bala, self.y_bala, 3, 10)
 
+        self.lst_colliders = []
+        self.hitting_lst = []
+
     def gerar_posicao(self):
           for i in range(self.qtd_canhao):
-            #vai gerar a posicao x do coracao no mapa
             index = random.randint(0,len(self.y_possiveis) - 1)
             self.x_balas.append(7)
+            self.hitting_lst.append(False)
+            self.lst_colliders.append(self.collider_bala)
             self.y.append(self.y_possiveis[index])
             self.y_possiveis.remove(self.y_possiveis[index])
-            velo = random.randint(5,8)
+            velo = random.randint(2,8)
             self.velocidades.append(velo)
 
-    def atirar(self, val, collider):
-        collider_bala = pygame.Rect(self.x_balas[val] + 7.5, self.y[val] + 30, 10, 10)
+    def atirar(self, val):
         if(self.atirando):
             self.x_balas[val] += self.velocidades[val]
-            if(self.x_balas[val] >= 395):
+            if(self.x_balas[val] >= 415):
                 self.x_balas[val] = 7
-            elif(collider_bala.colliderect(collider)):
-                self.x_balas[val] += 20
     
         
     def desenhar(self, val):
@@ -49,13 +49,19 @@ class Canhao:
         
         
     def canhao_dano(self, vida, colisor, val):
-        collider_bala = pygame.Rect(self.x_balas[val] + 7.5, self.y[val] + 30, 10, 10)
-        if(collider_bala.colliderect(colisor) and not self.hitting):
+        self.lst_colliders[val] = pygame.Rect(self.x_balas[val] + 7.5, self.y[val] + 30, 10, 10)
+        if(self.lst_colliders[val].colliderect(colisor) and self.hitting_lst[val] == False):
             vida -= 1
+            self.hitting_lst[val] = True
+        elif(not self.lst_colliders[val].colliderect(colisor)):
+            self.hitting_lst[val] = False
+
         return vida
 
     def resetar(self):
         self.x = -20
+        self.lst_colliders = []
+        self.hitting_lst = []
         self.y = []
         self.x_balas = []
         self.y_possiveis = [575, 475,375,275,175,75]
